e823916102a54d91e42513ca0d06c2189e58dd19
Stone Blair <blairs2@spu.edu>
2020-04-28 18:46:53 -0700

    Rudimentary search, apple login, apple music playlist library displayed [MM-17] [MM-11]

--- modules/user.js
+++ modules/user.js
@@ -1,16 +1,16 @@
-var db = require('../db');
-var passwordHash = require('password-hash');
-
-
-
-exports.createNewUser = function InsertUserHandler(username, password, done){
-  var hashedPassword = passwordHash.generate(password);
-  var vlaues = [username, hashedPassword];
-    db.get().query(
-      'INSERT INTO User (username, password)' +
-      'VALUES (?)', values, function  function InsertQueryHandler(err, result){
-                  if (err)
-                      return done(err);
-                  done(null, result.insertId);
-              });
-    }
+// var db = require('../db');
+// var passwordHash = require('password-hash');
+//
+//
+//
+// exports.createNewUser = function InsertUserHandler(username, password, done){
+//   var hashedPassword = passwordHash.generate(password);
+//   var vlaues = [username, hashedPassword];
+//     db.get().query(
+//       'INSERT INTO User (username, password)' +
+//       'VALUES (?)', values, function  function InsertQueryHandler(err, result){
+//                   if (err)
+//                       return done(err);
+//                   done(null, result.insertId);
+//               });
+//     }
