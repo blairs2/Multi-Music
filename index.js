e3b3f80072188e424799c5e4d403ee8b1f3af23a
Elizabeth Myers <myerse1@spu.edu>
2020-05-05 18:28:57 -0700

    Change page change to div update

---
+++ index.js
@@ -0,0 +1,71 @@
+function homeLoad() {
+    document.getElementById("page").innerHTML =
+         '<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel"></div>' +
+         '<h2 class="my-4">Home</h2>' +
+         '<div>' +
+         '<h5>Discover</h5>' +
+         '<div class="row">' +
+         '<div class="col-lg-3 col-md-6 mb-4">' +
+         '<div class="card h-100">' +
+         '<a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>' +
+         '<div class="card-body">' +
+            '<h4 class="card-title">' +
+             '<a href="#">Playlist One</a>' +
+            '</h4>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '<div class="col-lg-3 col-md-6 mb-4">' +
+         '<div class="card h-100">' +
+         '<a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>' +
+         '<div class="card-body">' +
+            '<h4 class="card-title">' +
+             '<a href="#">Playlist Two</a>' +
+            '</h4>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '<div class="col-lg-3 col-md-6 mb-4">' +
+         '<div class="card h-100">' +
+         '<a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>' +
+         '<div class="card-body">' +
+            '<h4 class="card-title">' +
+             '<a href="#">Playlist Three</a>' +
+            '</h4>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>' ;
+}
+
+function playlistLoad() {
+    document.getElementById("page").innerHTML =
+         '<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel"></div>' +
+         '<h2 class="my-4">Playlists</h2>' +
+         '<div>' +
+         '<h5>My Music</h5>' +
+         '<div class="row">' +
+         '<div class="col-lg-3 col-md-6 mb-4">' +
+         '<div class="card h-100">' +
+         '<a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>' +
+         '<div class="card-body">' +
+            '<h4 class="card-title">' +
+             '<a href="#">Playlist One</a>' +
+            '</h4>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '<div class="col-lg-3 col-md-6 mb-4">' +
+         '<div class="card h-100">' +
+         '<a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>' +
+         '<div class="card-body">' +
+            '<h4 class="card-title">' +
+             '<a href="#">Playlist Two</a>' +
+            '</h4>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>' +
+         '</div>' ;
+}
