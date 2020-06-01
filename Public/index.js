b2e6e011e5bb46b105b724b9072b9089cbbf9d33
Stone Blair <blairs2@spu.edu>
2020-05-31 21:10:22 -0700

    Done

--- Public/index.js
+++ Public/index.js
@@ -629,7 +629,7 @@ async function convertPlaylist(playlist_id, current_service, mm_playlist_id, pla
    var search, track, matches, handle;
    for(var i = 0; i < tracks.length; i++){
      track = tracks[i];
-     document.getElementById("convert-link").innerHTML = `<span>Loading playlist link${i}/${tracks.length}</span>`;
+     document.getElementById("convert-link").innerHTML = `<span>Loading playlist link ${i}/${tracks.length}</span>`;
      handle = await dbHasSong(track.id).then(async function(resp){
        if(resp == 'false'){
          //replace spaces with plus and get rid of special characters
